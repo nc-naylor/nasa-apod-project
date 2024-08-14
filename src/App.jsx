513023f79290d58fe9be5684be9becc8174aa1a3
nc-naylor <natasha.c.naylor@gmail.com>
2024-08-14 11:24:25 -0700

    Added a smoother transition to modal toggle, adjusted logic in App and Sidebar to allow additional class of open to be appended to sidebar class as necessary.

--- src/App.jsx
+++ src/App.jsx
@@ -13,7 +13,7 @@ function App() {
   return (
     <>
       <Main />
-      {showModal && <Sidebar handleToggleModal={handleToggleModal} />}
+      {<Sidebar isOpen={showModal} handleToggleModal={handleToggleModal} />}
       <Footer handleToggleModal={handleToggleModal} />
     </>
   );
--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,26 +1,29 @@
 import React from 'react';
 
 export default function Sidebar(props) {
-  const { handleToggleModal } = props;
+  const { isOpen, handleToggleModal } = props;
 
   return (
-    <div className='sidebar'>
-      <div onClick={handleToggleModal} className='bgOverlay'></div>
-      <div className='sidebarContents'>
-        <h2>The Beautiful Carina Nebula</h2>
-        <div>
-          <p>Description</p>
-          <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus,
-            nostrum cupiditate sunt quis magni nesciunt assumenda est sequi!
-            Perferendis itaque tempora cupiditate optio labore ratione porro
-            laborum assumenda, illo aut.
-          </p>
+    <>
+      <div className={`sidebar ${isOpen ? 'open' : ''}`}>
+        <div onClick={handleToggleModal} className='bgOverlay'></div>
+        <div className='sidebarContents'>
+          <h2>The Beautiful Carina Nebula</h2>
+          <div>
+            <p>Description</p>
+            <p>
+              Lorem ipsum dolor sit amet consectetur adipisicing elit. Ducimus,
+              nostrum cupiditate sunt quis magni nesciunt assumenda est sequi!
+              Perferendis itaque tempora cupiditate optio labore ratione porro
+              laborum assumenda, illo aut.
+            </p>
+          </div>
+          <button onClick={handleToggleModal}>
+            <i className='fa-solid fa-circle-chevron-right'></i>
+          </button>
         </div>
-        <button onClick={handleToggleModal}>
-          <i className='fa-solid fa-circle-chevron-right'></i>
-        </button>
       </div>
-    </div>
+      {isOpen && <div className='bgOverlay' onClick={handleToggleModal}></div>}
+    </>
   );
 }
